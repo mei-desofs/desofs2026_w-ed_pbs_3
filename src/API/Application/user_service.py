05614826dc215118c3b5b4e3dd5565351d8aef70
Eduardo Rafael Nunes Sampaio <eduardorafaelnunessampaio@MacBook-Air-de-Eduardo.local>
2026-05-09 16:36:21 +0100

    Implementação de Refresh de Tokens

--- src/API/Application/user_service.py
+++ src/API/Application/user_service.py
@@ -1,19 +1,32 @@
 from flask import Flask, jsonify, request
 import json
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, create_refresh_token
 
 class AuthenticationError(Exception):
     pass
 
 class UserService:
-    def authenticate(self, email_str, password_raw) -> json:
+    def authenticate(self, email_str, password_raw) -> tuple[str, str]:
         """
         Valida credencias de login
         ARGS: email_str (str), password_raw(str)
-        returns: token caso a validação seja bem sucedida
+        returns: token de acesso e refresh token caso a validação seja bem sucedida
         """
+
+        #!! Verificação manual a ser alterada
         if email_str == 'admin' and password_raw == 'password':
-            token = create_access_token(identity=email_str)
-            return token
+            a_token = create_access_token(identity=email_str)
+            r_token = create_refresh_token(identity=email_str)
+            return a_token, r_token
         else:
             raise AuthenticationError()
+        
+    def refreshtoken(self, email_str) -> str:
+        """
+        Atualiza o token a JWT
+        ARGS: email_str(str)
+        returns: token
+        """
+
+        new_token = create_access_token(identity=email_str)
+        return new_token
