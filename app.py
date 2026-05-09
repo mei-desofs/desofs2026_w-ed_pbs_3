4b32793a6a798ed9c66e365ce103fb1658bdd079
Eduardo Rafael Nunes Sampaio <eduardorafaelnunessampaio@MacBook-Air-de-Eduardo.local>
2026-05-09 16:36:21 +0100

    Implementação de Refresh de Tokens

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, request, jsonify, blueprints
 from flask_jwt_extended import JWTManager
 from src.API.user_routes import user_bp
+from datetime import timedelta
 
 
 app = Flask(__name__)
 
 # inicialização JWT
 app.config["JWT_SECRET_KEY"] = "super-secret-key"
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=15)
+app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
+app.config["JWT_COOKIE_HTTPONLY"] = True
+app.config["JWT_COOKIE_SAMESITE"] = "Strict"
+app.config["JWT_REFRESH_COOKIE_PATH"] = "/refresh"
+app.config["JWT_ACCESS_COOKIE_PATH"] = "/"
+# !!!!! Definir TRUE quando for usado HTTPS
+app.config["JWT_COOKIE_SECURE"] = False
+
 jwt = JWTManager(app)
 
 # definição de blueprints
--- src/API/user_routes.py
+++ src/API/user_routes.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify, request, Blueprint
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
-import json
+from flask_jwt_extended import unset_access_cookies, jwt_required, get_jwt_identity, set_access_cookies, set_refresh_cookies
 from src.API.Application.user_service import UserService, AuthenticationError
 
 user_service = UserService()
@@ -10,11 +9,30 @@ user_bp = Blueprint("users",__name__)
 def user_login():
     data = request.json
     try:
-        token = user_service.authenticate(data['email'], data['password'])
+        resp = jsonify({"msg": "Login Successful"})
+        a_token, r_token = user_service.authenticate(data['email'], data['password'])
+        set_access_cookies (resp, a_token)
+        set_refresh_cookies (resp, r_token)
 
-        return jsonify({"access_token": token}), 200
+        return resp, 200
     except AuthenticationError:
         return jsonify({"error": "Credenciais inválidas"}), 401
+
+
+#É necessário criar função JS que chame este endpoint para atualizar o token
+@user_bp.route('/refresh', methods=['POST'])
+@jwt_required(refresh=True)
+def refresh():
+
+    identity = get_jwt_identity()
+
+    new_token = user_service.refreshtoken(identity)
+
+    resp = jsonify({"msg": "token refreshed"})
+
+    set_access_cookies(resp, new_token)
+
+    return resp
     
 @user_bp.route('/mainpage', methods = ['POST'])
 @jwt_required()
@@ -25,3 +43,21 @@ def mainpage():
     
     except AuthenticationError:
         return jsonify({"error": "Token not found"}), 401
+    
+@user_bp.route('/logout',methods = ['POST'])
+@jwt_required()
+def logout():
+    resp = jsonify({"msg": "Logout Successful"})
+    unset_access_cookies
+    return resp, 200
+
+
+@user_bp.route('/profile', methods=['POST'])
+@jwt_required()
+def profile():
+
+    current_user = get_jwt_identity()
+
+    return jsonify({
+        "logged_as": current_user
+    }), 200
