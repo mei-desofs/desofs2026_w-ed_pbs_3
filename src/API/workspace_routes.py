764456eb93279694de2f3b779c7ec3c1e94ef591
Abilio Correia <abiliocorreia473@gmail.com>
2026-06-12 13:22:50 +0100

    Implementação de flask-limiter para o rate limiting

--- src/API/workspace_routes.py
+++ src/API/workspace_routes.py
@@ -1,43 +1,50 @@
 import uuid
 import requests
-import time
+import os
 
 from flask import Blueprint, jsonify, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
 from src.infrastructure.persistance.workspace_repository import WorkspaceRepository
 from src.domain.workspace.workspace_name import validate_workspace_name, InvalidWorkspaceNameError
-import os
 
 workspace_bp = Blueprint("workspace", __name__)
 repo = WorkspaceRepository()
 
 WORKSPACE_URL = "http://workspace:8000/workspace"
 
-# anti-spam
-workspace_creation_tracker = {}
-
 SERVICE_TOKEN = os.getenv("WORKSPACE_SERVICE_TOKEN")
 
 
+# FLASK LIMITER (JWT BASED)
+def get_user_id():
+    return str(get_jwt_identity())
+
+
+limiter = Limiter(
+    key_func=get_user_id
+)
+
+
+# CREATE WORKSPACE
 @workspace_bp.route("/workspaces/create", methods=["POST"])
 @jwt_required()
+@limiter.limit("10 per minute")
 def create_workspace():
 
     user_id = get_jwt_identity()
 
-    # RATE LIMIT (ANTI-SPAM)
-    now = time.time()
-    last = workspace_creation_tracker.get(user_id)
+    # INPUT
+    data = request.get_json()
+    name = data.get("name")
 
-    if last and now - last < 3:
-        return jsonify({
-            "error": "Too many requests. Please wait a few seconds."
-        }), 429
+    if not name:
+        return jsonify({"error": "Workspace name required"}), 400
 
-    workspace_creation_tracker[user_id] = now
-
-    # QUOTA CHECK
+    # QUOTA CHECK (max 10 workspaces)
     current_count = repo.count_by_user(user_id)
 
     if current_count >= 10:
@@ -45,15 +52,7 @@ def create_workspace():
             "error": "Workspace limit reached (max 10 per user)"
         }), 403
 
-    # INPUT
-    data = request.get_json()
-
-    name = data.get("name")
-
-    if not name:
-        return jsonify({"error": "Workspace name required"}), 400
-
-    # VALIDATION (DOMAIN)
+    # DOMAIN VALIDATION
     try:
         name = validate_workspace_name(name)
     except InvalidWorkspaceNameError as e:
@@ -78,6 +77,9 @@ def create_workspace():
         },
         headers={
             "X-Service-Token": SERVICE_TOKEN
+        },
+        cookies={
+            "access_token_cookie": access_cookie
         }
     )
 
@@ -102,6 +104,7 @@ def create_workspace():
     })
 
 
+# LIST WORKSPACES
 @workspace_bp.route("/workspaces", methods=["GET"])
 @jwt_required()
 def list_workspaces():
