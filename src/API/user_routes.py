2b121d52f74a6599f83375c42e197a794ce27ec8
Eduardo Rafael Nunes Sampaio <eduardorafaelnunessampaio@MacBook-Air-de-Eduardo.local>
2026-06-13 17:19:29 +0100

    Correção de métodos http utilizados em user_routes

--- src/API/user_routes.py
+++ src/API/user_routes.py
@@ -79,8 +79,7 @@ def mainpage():
     if request.method == 'GET':
         return render_template('/mainpage.html')
 
-#Talvez venha a remover, serve apenas para testar a mainpage
-@user_bp.route('/get_current_user')
+@user_bp.route('/get_current_user', methods=['GET'])
 @jwt_required(optional=True)
 def get_current_user():
     """
@@ -103,28 +102,6 @@ def get_current_user():
         "logged_as": user._username
     }), 200
 
-@user_bp.route('/register', methods=['POST', 'GET'])
-def register():
-    if request.method == 'GET':
-        return render_template('/register.html')
-
-    data = request.get_json()
-    
-    try:
-        # O serviço vai chamar o User.create(), que chama o UserName.validate()
-        user_service.register_user(data['username'], data['password'])
-        return jsonify({"msg": "Utilizador criado com sucesso!"}), 201
-
-    except (InvalidUserNameError, LengthError, RockError) as e:
-        # Captura de Errors específicos do Value Object
-        return jsonify({"error": str(e)}), 400
-
-    except Exception as e:
-        # Um "catch-all" para erros inesperados (ex: BD em baixo)
-        # Aqui enviamos 500 porque o erro é do nosso lado
-        return jsonify({"error": "Erro interno no servidor"}), 500
-
-#TODO Rever logout
 @user_bp.route('/logout', methods=['POST'])
 @jwt_required()
 def logout():
